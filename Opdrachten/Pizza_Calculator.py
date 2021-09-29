9d8a6e907cc55b409b856ce17431a3e18fb323d4
Mert Erciyas <99058235@mydavinci.nl>
2021-09-29 11:27:02 +0200

    Pizza Calculator Verbetering

--- Opdrachten/Pizza_Calculator.py
+++ Opdrachten/Pizza_Calculator.py
@@ -3,10 +3,15 @@ smallPizza = int(input("Hoeveel kleine pizzas wilt u?\n")) # hierbij kan je invo
 mediumPizza = int(input("Hoeveel medium pizzas wilt u?\n")) # hierbij kan je invoeren hoeveel medium pizzas je wilt
 largePizza = int(input("Hoeveel grote pizzas wilt u?\n")) # hierbij kan je invoeren hoeveel large pizzas je wilt
 
-smallPizzaPrice = float(7.50) # hier zie je de prijs van de small pizza
-mediumPizzaPrice = float(9.50) # hier zie je de prijs van de medium pizza
-largePizzaPrice = float(11.50) # hier zie je de prijs van de large pizza
+smallPizzaPrice = 7.50 # hier zie je de prijs van de small pizza
+mediumPizzaPrice = 9.50 # hier zie je de prijs van de medium pizza
+largePizzaPrice = 11.50 # hier zie je de prijs van de large pizza
 
-print("Dus jij hebt" , smallPizza , "small pizzas" , mediumPizza , "medium pizzas" , largePizza , "large pizzas\n Dat word dan:") # hierbij laat hij weten hoeveel pizzas je hebt besteld
-kosten = (int((smallPizza * smallPizzaPrice) + (mediumPizza * mediumPizzaPrice) + (largePizza * largePizzaPrice))) # hier berekend hij de kosten van alle pizzas
+smallPizzaKosten = (smallPizza * smallPizzaPrice)
+mediumPizzaKosten = (mediumPizza * mediumPizzaPrice)
+largePizzaKosten = (largePizza * largePizzaPrice)
+
+print("Dus jij hebt" , smallPizza , "small pizzas" , mediumPizza , "medium pizzas" , largePizza , "large pizzas\n") # hierbij laat hij weten hoeveel pizzas je hebt besteld
+print("dat word dan" , smallPizzaKosten , "euro voor de small pizza's" , mediumPizzaKosten , "euro voor de medium pizza's" , largePizzaKosten , "euro voor de large pizza's\n Dat word dan in totaal:" )
+kosten = (float((smallPizza * smallPizzaPrice) + (mediumPizza * mediumPizzaPrice) + (largePizza * largePizzaPrice))) # hier berekend hij de kosten van alle pizzas
 print(kosten , "euro") # hier laat hij zien hoeveel de pizzas in totaal kosten
